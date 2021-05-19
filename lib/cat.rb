1d0eded846e20b349f592c0fb3388bc51315bd10
Zach Frohardt <zfrohardt@gmail.com>
2021-05-19 15:36:51 -0700

    commit tree e15ed18d5bb7ae8b916625bd8ae4546fd3e9bb55

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
 class Cat
-  # code goes here
+    attr_reader :name
+    attr_accessor :owner, :mood
+
+    @@all = []
+
+    def initialize(name, owner)
+        @name = name
+        @mood = "nervous"
+        @owner = owner
+        @@all.push self
+    end
+
+    def self.all
+        @@all
+    end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,35 @@
 class Owner
-  # code goes here
+    attr_reader :name, :species
+    
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @species = "human"
+        @@all.push self
+    end
+
+    def say_species
+        "I am a #{self.species}."
+    end
+
+    def cats
+        Cat.all.select { |cat| cat.owner == self }
+    end
+
+    def dogs
+        Dog.all.select { |cat| cat.owner == self }
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.count
+        @@all.count
+    end
+
+    def self.reset_all
+        @@all = []
+    end
 end
